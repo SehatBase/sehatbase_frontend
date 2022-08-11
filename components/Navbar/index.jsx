28c6fd8bc7766c183e8bc3e10c6206ed406aadf2
Asadullah Kashif <s.asad18k@gmail.com>
2022-08-11 13:43:24 +0500

    demo buttons and links w/ palette config

---
+++ components/Navbar/index.jsx
@@ -0,0 +1,13 @@
+import { Box, Button, Link, Typography, useTheme } from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <Box>
+      <Link href="#">Default Link</Link>
+      <br />
+      <Button>Default Button</Button>
+    </Box>
+  );
+};
+
+export default Navbar;
