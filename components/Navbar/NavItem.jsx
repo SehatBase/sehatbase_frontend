4246d9f88c6d54166a3ccca14c83b7fcdafefce7
Asadullah Kashif <s.asad18k@gmail.com>
2022-08-15 12:43:27 +0500

    Navbar Complete

---
+++ components/Navbar/NavItem.jsx
@@ -0,0 +1,44 @@
+import { Link, Typography, useTheme } from "@mui/material";
+import { Box } from "@mui/system";
+import { useRouter } from "next/router";
+import React from "react";
+import { hrefToText } from "../../utils/NavLinksHelper";
+
+const NavItem = ({ text, href }) => {
+  const theme = useTheme();
+  const router = useRouter();
+
+  const isActive = hrefToText(router.pathname) === text;
+
+  return (
+    <Box
+      sx={{ ":hover": { opacity: "80%" } }}
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Link href={href}>
+        <Typography
+          color={"primary"}
+          textTransform={"none"}
+          variant="body2"
+          sx={{
+            cursor: "pointer",
+          }}
+        >
+          {text}
+        </Typography>
+      </Link>
+      {isActive && (
+        <Box
+          sx={{
+            background: theme.palette.secondary.main,
+            height: "3px",
+          }}
+        ></Box>
+      )}
+    </Box>
+  );
+};
+
+export default NavItem;
--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,12 +1,78 @@
-import { Box, Button, Link, Typography, useTheme } from "@mui/material";
+import { Box, Button, Link } from "@mui/material";
+import { Container } from "@mui/system";
+import NavItem from "./NavItem";
+
+export const navItems = {
+  links: [
+    {
+      text: "Home",
+      href: "/",
+      isActive: true,
+    },
+    {
+      text: "Blog",
+      href: "/blog",
+    },
+    {
+      text: "About Us",
+      href: "/about-us",
+    },
+    {
+      text: "FAQs",
+      href: "/faqs",
+    },
+    {
+      text: "Contact",
+      href: "/contact",
+    },
+  ],
+  buttons: [
+    {
+      text: "Book An Appointment",
+      href: "/book-appointment",
+    },
+  ],
+};
 
 const Navbar = () => {
   return (
-    <Box>
-      <Link href="#">Default Link</Link>
-      <br />
-      <Button>Default Button</Button>
-    </Box>
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingY: "1rem",
+      }}
+    >
+      <Box>
+        <Link href="/">Brand</Link>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          width: "60%",
+        }}
+      >
+        {navItems.links.map(item => (
+          <NavItem key={item.text} {...item} />
+        ))}
+
+        <Box sx={{ marginLeft: "2.5rem" }}>
+          {navItems.buttons.map(item => (
+            <Button
+              key={item.text}
+              size="medium"
+              sx={{ paddingX: "1.5rem", textTransform: "none" }}
+            >
+              <Link href={item.href}>{item.text}</Link>
+            </Button>
+          ))}
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
