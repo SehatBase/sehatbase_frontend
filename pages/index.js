ed1b506d8741bba5a9d0ec37d786fd25186bf5a6
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2022-08-15 10:59:05 +0500

    index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
-
+import Faq from '../comps/Faq';
 
 export default function Home() {
   return (
-  );
+    <div className={styles.container}>
+      SehatBase
+    </div>
+  )
 }
