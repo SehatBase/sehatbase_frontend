09b41f915b07138461cd9f96989cdb0f3b306b2c
Asadullah Kashif <s.asad18k@gmail.com>
2022-08-16 20:08:54 +0500

    changed background color of sidebar to darker shade of purple

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -96,7 +96,7 @@ const Navbar = () => {
           right: 0,
           overflow: "hidden",
           zIndex: 100,
-          background: theme.palette.primary.light,
+          background: theme.palette.primary.dark,
           width: "max(33.3%, 300px)",
           height: "75vh",
           display: "flex",
