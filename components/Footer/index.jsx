c8c2c7ffb8bdc4e758711956342a06a50dd0d05e
Asadullah Kashif <s.asad18k@gmail.com>
2022-08-17 02:02:48 +0500

    removed an unused import

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,7 +1,6 @@
 import { useTheme } from "@emotion/react";
 import { useMediaQuery } from "@mui/material";
 import { Box, Container } from "@mui/system";
-import Image from "next/image";
 import FooterBottom from "./FooterBottom";
 import FooterLogo from "./FooterLogo";
 import LinksList from "./LinksList";
