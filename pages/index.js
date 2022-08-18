2efbed57608591e191f45867735274dd4f816560
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2022-08-18 12:45:56 +0500

    merge conflict fix

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,9 @@ import { ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import Navbar from "../components/Navbar";
 import { muiTheme } from "../config/MuiConfig";
-import ContactUs from '../components/ContactUs';
 
 export default function Home() {
   return (
-    <>
-    {/* <Navbar /> */}
-    <ContactUs />
-    </>
+    <Navbar />
   );
 }
