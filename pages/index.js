2c49249b62d8a23f927ebf4ad46547c9717a0abc
ramisha-anwer <ramishaaa27@gmail.com>
2022-08-22 17:11:18 +0500

    resolved conflicts

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import { ThemeProvider } from '@mui/material/styles';
 import React from 'react';
+import Features from '../components/Features/features';
 import Navbar from "../components/Navbar";
 import { muiTheme } from "../config/MuiConfig";
+
 
 
 export default function Home() {
   return (
     <>
     <Navbar />
+    <Features/>
     </>
     
   );
