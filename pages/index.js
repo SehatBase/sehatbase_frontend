249adb85fdaa247ac81aa248b2978e070118ae53
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2022-09-07 11:29:15 +0500

    conflict resolve

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-// import Navbar from "../components/Navbar";
-// import HeroSection from '../components/HeroSection';
-import Newsletter from '../components/Modal/Newsletter';
+import Features from '../components/Features/features';
+import Navbar from "../components/Navbar";
+import HeroSection from '../components/HeroSection';
+
+
 
 export default function Home() {
   return (
     <>
-      <Newsletter />
+      <Features/>
     </>
   );
 }
